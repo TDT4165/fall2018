72c7227a0183dc9920d3daebb9e35ef863624f87
fishyFrogFace <camillmg@stud.ntnu.no>
2018-08-29 09:30:34 +0200

    Skeleton for exercise 2

---
+++ ex2code/src/Lib.hs
@@ -0,0 +1,102 @@
+module Lib
+    ( f0
+    , f1
+    , f2
+    , take
+    , map
+    , iterate
+    , filterPos
+    , filterPosMany
+    , Maybe(..)
+    , safeHeadList
+    , safeHead
+    ) where
+
+import Prelude hiding (map, take, iterate, sqrt, Maybe)
+
+-- TASK 1
+-- Parametric polymorphism
+
+-- Below are three type signatures. Can you implement them? We
+-- say a function or implementation /inhabits/ it's type
+-- (signature). How many other inhabitants do these types
+-- have? What if we fixed a = Int, does that change your
+-- previous answer?
+f0 :: a -> a
+f0 = undefined
+
+f1 :: a -> b -> a
+f1 = undefined
+
+f2 :: a -> b -> b
+f2 = undefined
+
+-- Rewrite the function "takeInt" from exercice 1 as "take" so
+-- that it accepts a list of any type. If you used the
+-- built-in function "take" on the last assignment, write your
+-- own implementation this time. Be sure to include a type
+-- signature. (Hint: If you already wrote takeInt, you won't
+-- have to change much.)
+take = undefined
+
+-- The function head :: [a] -> a which returns the first
+-- element of a list, is /partial/, meaning it will crash for
+-- some inputs. (Which?) One solution could be to make a
+-- /total/ function "safeHeadList :: [a] -> [a]" which either
+-- gives the head, or nothing. Can you implement it using take?
+safeHeadList :: [a] -> [a]
+safeHeadList = undefined
+
+-- The output of safeHeadList is either empty or a singleton,
+-- and thus using a list as output-type is a bit misleading. A
+-- better choice is Maybe (sometimes called Optional):
+data Maybe a = Some a | None deriving (Eq, Show)
+
+-- Implement 'safeHead', representing failure using None.
+safeHead :: [a] -> Maybe a
+safeHead = undefined
+
+-- TASK 2
+-- Higher order functions
+
+map :: (a -> b) -> [a] -> [b]
+map = undefined
+
+iterate :: (a -> a) -> a -> [a]
+iterate = undefined
+
+-- TASK 3
+-- Currying and partial application
+
+-- complete the function filterPos
+-- that takes a list and returns 
+-- a filtered list containing only positive
+-- integers (including zero)
+-- use partial application to achieve this
+filterPos :: [Int] -> [Int]
+filterPos = undefined
+
+-- complete the function filterPosMany
+-- that takes a list of lists and returns
+-- a list of lists with only positive
+-- integers (including zero)
+-- hint: use filterPos and map
+filterPosMany :: [[Int]] -> [[Int]]
+filterPosMany = undefined
+
+flip3 :: (a -> b -> c -> d) -> c -> b -> a -> d
+flip3 = undefined
+
+-- TASK 4
+-- Infinite lists
+
+isPerfSq :: Double -> Bool
+isPerfSq = undefined
+
+--uncomment when isPerfSqr is defined
+--accuracy :: Int -> Bool
+--accuracy x = take x generated == take x [x^2 | x <- [1..]]
+--                where
+--             zpd       = zip [1..] (map isPerfSq [1..])
+--             f (x,y)   = y == True
+--             generated = fst . unzip $ filter f zpd
---
+++ ex2code/test/Spec.hs
@@ -0,0 +1,85 @@
+import Test.Hspec
+import Test.QuickCheck
+
+import Lib as L
+
+genNeg :: Gen Int
+genNeg = choose (-25, -1)
+
+genPos :: Gen Int
+genPos = choose (0, 12000)
+
+negList = [-10..(-1)]
+mixList = [-10, -4, 347, -9, 0, 0, 2, -2, 0, 1, -27]
+manyCheck = [[], negList, mixList]
+
+main :: IO ()
+main = hspec $ do
+
+    describe "f0" $ do
+        it "returns a String for a String input" $ do
+            L.f0 "a string" `shouldBe` Prelude.id "a string"
+        it "returns a Bool for a Bool input" $ do
+            L.f0 True `shouldBe` Prelude.id True
+        it "returns a Double for a Double input" $ do
+            L.f0 (3.0 :: Double) `shouldBe` Prelude.id (3.0 :: Double)
+
+    describe "f1" $ do
+        it "returns a String for a String -> Int input" $ do
+            L.f1 "monkey" 5 `shouldBe` "monkey"
+        it "returns a Bool for a Bool -> String input" $ do
+            L.f1 True "monkey" `shouldBe` True
+
+    describe "f2" $ do        
+        it "returns an Int for a String -> Int input" $ do
+            L.f2 "monkey" 5 `shouldBe` 5
+        it "returns a String for a Bool -> String input" $ do
+            L.f2 True "monkey" `shouldBe` "monkey"
+
+    describe "take" $ do
+        it "returns an empty list for negative integers" $ do
+            property $ forAll genNeg $ \n -> L.take n [1..10] == ([]:: [Int])
+        it "returns an empty list for n = 0" $ do
+            L.take 0 ['a'..'z'] `shouldBe` ([] :: [Char])
+        it "returns an empty list for []" $ do
+            L.take 5 [] `shouldBe` ([] :: [Int])
+        it "returns the n first elements of a list" $ do
+            L.take 5 ['a'..'z'] `shouldBe` ['a'..'e']
+        it "returns the whole list if it's shorter than n" $ do
+            L.take 10 [1..5] `shouldBe` [1..5]
+
+    describe "safeHeadList" $ do
+      it "returns empty list for empty list" $ do
+        L.safeHeadList ([] :: [Int]) `shouldBe` ([] :: [Int])
+      it "returns singleton consisting of first element of non-empty list" $ do
+        L.safeHeadList [1..10] `shouldBe` [1]
+
+    describe "safeHead" $ do
+      it "returns None for empty list" $ do
+        L.safeHead [] `shouldBe` (L.None :: L.Maybe Int)
+      it "returns Some of head of non-empty list" $ do
+        L.safeHead [1..10] `shouldBe` L.Some 1
+
+    describe "map" $ do
+        it "returns an empty list for f []" $ do
+            L.map (+1) [] `shouldBe` []
+        it "applies a function to all elements" $ do
+            L.map (+2) [1..10] `shouldBe` [3..12]
+
+    describe "iterate" $ do
+        it "creates a list of multiples of 5" $ do
+            property $ forAll genPos $ \n -> (L.iterate (+5) 0) !! n == n*5
+        it "creates a list where each element prepends space to a string" $ do
+            property $ forAll genPos $ \n -> (L.iterate (' ':) "") !! n == replicate n ' '
+
+    describe "filterPos" $ do
+        it "returns empty list for empty list" $ do
+            filterPos [] `shouldBe` []
+        it "returns empty list for list of negative integers" $ do
+            filterPos negList `shouldBe` []
+        it "filters out negative entries of a list" $ do
+            filterPos mixList `shouldBe` [347, 0, 0, 2, 0, 1]
+
+    describe "filterPosMany" $ do
+        it "filters all lists in a list" $ do
+            filterPosMany manyCheck `shouldBe` [[], [], [347, 0, 0, 2, 0, 1]]
